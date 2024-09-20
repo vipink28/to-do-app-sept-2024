8a1b0a25608b2b7aae47a1f16c7e044e04048307
vipink28 <vipinkalyan28@gmail.com>
2024-09-20 12:10:54 +0530

    login function added

--- src/auth/Login.jsx
+++ src/auth/Login.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login(props) {
+    const [formData, setFormData] = useState(null);
+    const handleChange = (e) => {
+        let { name, value } = e.target;
+        setFormData((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
+
+    const handleSubmit = async () => {
+        const response = await fetch(`http://localhost:5000/users?email=${formData.email}&password=${formData.password}`, { method: "GET" });
+        const users = await response.json();
+        if (users.length > 0) {
+            alert("user found");
+        } else {
+            alert("email/password incorrect");
+        }
+    }
+
     return (
         <div className='py-2'>
-
             <div className='mb-3'>
                 <label className='form-label'>Email</label>
-                <input type='email' name='email' className='form-control' />
+                <input type='email' name='email' className='form-control' onChange={handleChange} />
             </div>
 
             <div className='mb-3'>
                 <label className='form-label'>Password</label>
-                <input type='password' name='password' className='form-control' />
+                <input type='password' name='password' className='form-control' onChange={handleChange} />
             </div>
 
-            <button className='btn btn-primary'>Login</button>
+            <button onClick={handleSubmit} className='btn btn-primary'>Login</button>
 
         </div>
     );
--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -1,24 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Register(props) {
+    const [formData, setFormData] = useState(null);
+    const handleChange = (e) => {
+        let { name, value } = e.target;
+        setFormData((prev) => {
+            return {
+                ...prev,
+                [name]: value
+            }
+        })
+    }
+
+
+    const handleSubmit = async () => {
+        // api request in vanilla javascript
+        // in vanilla javascript we can use fetch() method to send request to an api
+        let config = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(formData)
+        }
+
+
+
+        const response = await fetch("http://localhost:5000/users", config);
+        if (response.status === 201) {
+            alert("successfully registered");
+        } else {
+            alert("something went wrong");
+        }
+    }
+
+
+
     return (
         <div className='py-2'>
             <div className='mb-3'>
                 <label className='form-label'>User Name</label>
-                <input type='text' name='username' className='form-control' />
+                <input type='text' name='username' className='form-control' onChange={handleChange} />
             </div>
 
             <div className='mb-3'>
                 <label className='form-label'>Email</label>
-                <input type='email' name='email' className='form-control' />
+                <input type='email' name='email' className='form-control' onChange={handleChange} />
             </div>
 
             <div className='mb-3'>
                 <label className='form-label'>Password</label>
-                <input type='password' name='password' className='form-control' />
+                <input type='password' name='password' className='form-control' onChange={handleChange} />
             </div>
 
-            <button className='btn btn-primary'>Register</button>
+            <button onClick={handleSubmit} className='btn btn-primary'>Register</button>
 
         </div>
     );
